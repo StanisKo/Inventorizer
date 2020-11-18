1df70ca2d41ec20a81e2ff95edde07c8fa959f1c
stan <stan@local>
2020-11-18 16:18:23 +0100

    some js

--- Inventorizer/wwwroot/js/site.js
+++ Inventorizer/wwwroot/js/site.js
@@ -1,4 +1,14 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+$(() => {
+    $("#datepicker").datepicker({});
+
+    $("#price").text((_, text) => `€ ${text}`);
+
+    $("#date-of-purchase").text((_, text) => {
+        const dateOfPurchase = new Date(text);
+
+        return `${dateOfPurchase.getDate()}-${dateOfPurchase.getMonth() + 1}-${dateOfPurchase.getFullYear()}`
+    });
+});
